d7acaf91641ba30e20bacc5d31701bde52dd7e02
nhicamvu <123439045+nhicamvu@users.noreply.github.com>
2023-03-12 23:57:14 -0500

    Update google-analytics-e-commerce-bigquery-export.sql

--- google-analytics-e-commerce-bigquery-export.sql
+++ google-analytics-e-commerce-bigquery-export.sql
@@ -1,4 +1,4 @@
-#Q1: calculate total visit, pageview, transaction for Jan, Feb and March 2017 (order by month)
+--Query 1: Total visit, pageview, transaction for Jan, Feb and March 2017 (order by month)
 SELECT distinct FORMAT_DATE("%Y%m", PARSE_DATE ("%Y%m%d",date)) AS month, -- k cần distinct, vì ở dưới mình group by thì nó cũng sẽ gom lại 1 dòng
   SUM(totals.visits) AS visits ,
   SUM(totals.pageviews) AS pageviews, 
@@ -8,20 +8,18 @@ where _table_suffix between '0101' and '0331'
   AND totals.transactions IS NOT NULL 
 GROUP BY 1
 ORDER BY month;
---correct
 
---Q2
+--Query 2: Bounce rate per traffic source in July 2017
 SELECT 
   distinct trafficSource.source, 
   SUM (totals.visits) AS total_visits,
   SUM (totals.bounces) AS total_no_of_bounces, 
-  SUM (totals.bounces/totals.visits) AS bounce_rate  --(sum(totals.Bounces)/sum(totals.visits))* 100 as bounce_rate
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*` --nếu data lấy tháng 7 thì mình có thể ghi ntn
---where _table_suffix between '0701' and '0731'                    --và bỏ phần này
+  SUM((totals.Bounces)/sum(totals.visits))* 100 as bounce_rate
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`
 GROUP BY 1
 ORDER BY total_visits DESC;
 
---Q3
+--Query 3: Revenue by traffic source by week, by month in June 2017
 SELECT 'month' AS time_type, 
     FORMAT_DATE("%Y%m", PARSE_DATE ("%Y%m%d",date)) AS time, 
     trafficSource.source,
@@ -41,10 +39,9 @@ UNNEST (hits) AS hits,
 UNNEST (hits.product) AS product
 WHERE product.productRevenue IS NOT NULL 
 GROUP BY 1,2,3;
---correct 
 
 
---Q4
+--Query 4: Average number of pageviews by purchaser type (purchasers vs non-purchasers) in June, July 2017.
 WITH purchaser_data as(
       SELECT 
           FORMAT_DATE("%Y%m", PARSE_DATE ("%Y%m%d",date)) AS month, 
@@ -80,9 +77,8 @@ SELECT
 FROM purchaser_data
 left join non_purchaser_data using(month)
 order by month;
---correct
 
---Q5
+--Query 5: Average number of transactions per user that made a purchase in July 2017
 SELECT 
     FORMAT_DATE("%Y%m", PARSE_DATE ("%Y%m%d",date)) AS month,  
     SUM(totals.transactions)/COUNT (distinct(fullVisitorID)) AS Avg_total_transactions_per_user
@@ -94,13 +90,11 @@ WHERE _table_suffix between '0701' and '0731'
   AND totals.totalTransactionRevenue IS NOT NULL
   AND product.productRevenue IS NOT NULL
 GROUP BY 1;
---correct
 
---Q6
+--Query 6: Average amount of money spent per session. Only include purchaser data in July 2017
 SELECT 
     FORMAT_DATE("%Y%m", PARSE_DATE ("%Y%m%d",date)) AS month,  
-    AVG ((product.productRevenue)/(totals.visits)) AS Avg_revenue_by_user_per_visit
-    --((sum(product.productRevenue)/sum(totals.visits))/power(10,6)) as avg_revenue_by_user_per_visit      --ghi như vậy sẽ hợp lý về mặt logic hơn
+    ((sum(product.productRevenue)/sum(totals.visits))/power(10,6)) as avg_revenue_by_user_per_visit      
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
 UNNEST (hits) AS hits,
 UNNEST (hits.product) AS product
@@ -109,7 +103,7 @@ WHERE _table_suffix between '0701' and '0731'
   AND product.productRevenue IS NOT NULL
 GROUP BY 1;
 
---Q7
+Query 7: Other products purchased by customers who purchased product "YouTube Men's Vintage Henley" in July 2017
 SELECT 
     product.v2productname AS other_purchased_product,	
     SUM(product.productQuantity) AS quantity
@@ -128,5 +122,3 @@ WHERE fullvisitorid in
 GROUP BY other_purchased_product
 ORDER BY quantity DESC;
 
-
---bài làm khá tốt, 1 số đoạn a chỉnh thụt dòng cho đẹp thoi
